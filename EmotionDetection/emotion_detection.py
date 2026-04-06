3d546348b324f99fa0f7088484c314042c4c65f2
efendi54 <aueret@gmail.com>
2026-04-06 13:28:57 -0400

    final project package creation

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse:str):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers=header)
+
+    formatted_response = json.loads(response.text)
+    emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+    report = {
+        'anger': emotion_predictions['anger'],
+        'disgust': emotion_predictions['disgust'],
+        'fear': emotion_predictions['fear'],
+        'joy': emotion_predictions['joy'],
+        'sadness': emotion_predictions['sadness'],
+        'dominant_emotion': 'anger'
+    }
+    dominant_emotion_score = report['anger']
+    for e in ['disgust', 'fear', 'joy', 'sadness']:
+        if report[e] > dominant_emotion_score:
+            report['dominant_emotion'] = e
+            dominant_emotion_score = report[e]
+    return report

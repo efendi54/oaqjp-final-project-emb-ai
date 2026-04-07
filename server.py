f5a3c481c0cde785fc53b4c1a4d7f4b2813f791e
efendi54 <aueret@gmail.com>
2026-04-07 03:12:26 -0400

    static code analysis

--- server.py
+++ server.py
@@ -1,16 +1,23 @@
+"""
+This is an example flask server implementation 
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-@app.route("/emotionDetector") 
-def emotion_detector_route(): 
+@app.route("/emotionDetector")
+def emotion_detector_route():
+    """
+    Example emotion detector routing function
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
     if response['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
-        
+
     report = "For the given statement, the system response is "
     report += f"'anger': {response['anger']}, "
     report += f"'disgust': {response['disgust']}, "
@@ -20,9 +27,12 @@ def emotion_detector_route():
     report += f"The dominant emotion is {response['dominant_emotion']}."
     return report
 
-@app.route("/") 
-def render_index_page(): 
+@app.route("/")
+def render_index_page():
+    """
+    Example rendering function
+    """
     return render_template('index.html')
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
